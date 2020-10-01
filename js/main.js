bc2d660cae1b6827b3590caf104cbe5504774fe9
Alexander Pankov <alexander.pankov88@mail.ru>
2020-10-01 18:51:50 +0700

    Рефакторинг кода. Добавлены Мокапы, исправлены замечания

--- js/main.js
+++ js/main.js
@@ -1,90 +1,106 @@
 'use strict';
 
-const COMMENTS_LIST = [
-  `Всё отлично!`,
-  `В целом всё неплохо. Но не всё.`,
-  `Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.`,
-  `Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.`,
-  `Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.`,
-  `Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!`];
-const AUTHOR_NAMES = [
-  `Александр`,
-  `Виктор`,
-  `Ирина`,
-  `Кирилл`,
-  `Ольга`,
-  `Петр`];
 const COMMENTS_MIN = 1;
 const COMMENTS_MAX = 4;
 const LIKES_MIN = 15;
 const LIKES_MAX = 200;
 const PHOTOS_COUNT = 25;
 
-let listOfPhotos = document.querySelector(`.pictures`);
+const COMMENTS_LIST = [
+  `Всё отлично!`,
+  `В целом всё неплохо. Но не всё.`,
+  `Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.`,
+  `Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.`,
+  `Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.`,
+  `Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!`
+];
 
-const picturesTemplate = document.querySelector(`#picture`).content;
+const AUTHOR_NAMES = [
+  `Александр`,
+  `Виктор`,
+  `Ирина`,
+  `Кирилл`,
+  `Ольга`,
+  `Петр`
+];
 
-
-const randomGenerator = function (length) {
+const getRandomGenerator = function (length) {
   return Math.floor(Math.random() * Math.floor(length));
 };
 
-const getPhoto = function (urlNumber) {
-  const likesCount = LIKES_MIN + randomGenerator(LIKES_MAX);
+const MockupPhotoObject = function (url, description, likes, comments) {
+  this.url = url;
+  this.description = description;
+  this.likes = likes;
+  this.comments = comments;
+};
 
-  const authorNumber = randomGenerator(AUTHOR_NAMES.length);
-  const author = AUTHOR_NAMES[authorNumber];
+const generateCommentContent = () => {
+  const commentNum = getRandomGenerator(COMMENTS_LIST.length);
+  let commentContent = COMMENTS_LIST[commentNum];
 
-  const commentsCount = randomGenerator(COMMENTS_MAX);
-  let comments = [];
-  let lastCommentNUmber = 0;
+  if (Math.random() < 0.5) {
+    const secondCommentNum = getRandomGenerator(COMMENTS_LIST.length);
+    commentContent += ` ` + COMMENTS_LIST[secondCommentNum];
+  }
+  return commentContent;
+};
 
-  for (let i = 0; i <= commentsCount; i++) {
-    let commentNumber = COMMENTS_MIN + randomGenerator(COMMENTS_LIST.length);
+const MockupComment = function () {
+  const avatarNum = getRandomGenerator(COMMENTS_MIN, 1, true);
+  const nameNum = getRandomGenerator(AUTHOR_NAMES.length);
+  const message = generateCommentContent();
 
-    if (commentNumber === lastCommentNUmber) {
-      i--;
-    } else {
-      comments.push(COMMENTS_LIST[commentNumber]);
-      lastCommentNUmber = commentNumber;
+  this.avatar = `img/avatar-${avatarNum}.svg`;
+  this.message = message;
+  this.name = AUTHOR_NAMES[nameNum];
+};
+
+const createPhotoArray = (arrayLength) => {
+  const photoArray = [];
+  for (let i = 1; i <= arrayLength; i++) {
+    const likesAmount = getRandomGenerator(LIKES_MAX, LIKES_MIN, true);
+    const commentsAmount = getRandomGenerator(COMMENTS_MAX);
+    const comments = [];
+    for (let j = 0; j < commentsAmount; j++) {
+      comments.push(new MockupComment());
     }
+    const newPhotoObject = new MockupPhotoObject(
+        `photos/${i}.jpg`,
+        `Тестовое фото №${i}`,
+        likesAmount,
+        comments
+    );
+    photoArray.push(newPhotoObject);
   }
-
-  let singlePhoto = {
-    url: `photos/` + urlNumber + `.jpg`,
-    description: author,
-    likes: likesCount,
-    comments};
-
-  return singlePhoto;
+  return photoArray;
 };
 
-const getAllPhotos = function (photosCount) {
-  let photos = [];
-  for (let i = 1; i <= photosCount; i++) {
-    photos.push(getPhoto(i));
+const createDomPictureElement = (template, pictureObject) => {
+  const newPictureElement = template.cloneNode(true);
+  const imgElement = newPictureElement.querySelector(`.picture__img`);
+  const likesAmountElement = newPictureElement.querySelector(`.picture__likes`);
+  const commentsAmountElement = newPictureElement.querySelector(`.picture__comments`);
+
+  imgElement.src = pictureObject.url;
+  likesAmountElement.textContent = pictureObject.likes;
+  commentsAmountElement.textContent = pictureObject.comments.length;
+  return newPictureElement;
+};
+
+const generateDomPicturesFragment = (picturesArray) => {
+  const newFragment = document.createDocumentFragment();
+  const pictureTemplate = document.querySelector(`#picture`).content.querySelector(`.picture`);
+  for (let i = 0; i < picturesArray.length; i++) {
+    const newChildElement = createDomPictureElement(pictureTemplate, picturesArray[i]);
+    newFragment.appendChild(newChildElement);
   }
-  return photos;
+  return newFragment;
 };
 
-const displayPicture = function (currentPhoto) {
-  const pictureTemplate = picturesTemplate.querySelector(`.picture`);
-  let picture = pictureTemplate.cloneNode(true);
+const picturesParentElement = document.querySelector(`.pictures`);
 
-  const img = picture.querySelector(`.picture__img`);
-  img.src = currentPhoto.url;
+const mockupPhotosArray = createPhotoArray(PHOTOS_COUNT);
 
-  const comments = picture.querySelector(`.picture__comments`);
-  comments.textContent = currentPhoto.comments;
-
-  const likes = picture.querySelector(`.picture__likes`);
-  likes.textContent = currentPhoto.likes;
-
-  return picture;
-};
-
-const allPhotos = getAllPhotos(PHOTOS_COUNT);
-
-for (let photo of allPhotos) {
-  listOfPhotos.appendChild(displayPicture(photo));
-}
+const mockupPhotosFragment = generateDomPicturesFragment(mockupPhotosArray);
+picturesParentElement.appendChild(mockupPhotosFragment);

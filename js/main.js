0e1865f9742c11f641692fd2a178e251e139c881
Alexander Pankov <alexander.pankov88@mail.ru>
2020-11-04 22:48:18 +0700

    Задание 4.13. Полноэкранный режим3

--- js/main.js
+++ js/main.js
@@ -30,10 +30,7 @@ const COMMENTS = {
   max: 10,
 };
 
-const AVATAR = {
-  width: 35,
-  height: 25,
-};
+const picturesElement = document.querySelector(`.pictures`);
 
 const getRandom = function (min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -72,10 +69,11 @@ const getMocksArray = function (objectsAmount) {
   return resultMocks;
 };
 
-const getPhoto = function (photo) {
+const getPhoto = function (photo, id) {
   const pictureTemplate = document.querySelector(`#picture`).content.querySelector(`.picture`);
   const pictureElement = pictureTemplate.cloneNode(true);
 
+  pictureElement.dataset.id = id;
   pictureElement.querySelector(`.picture__img`).src = photo.url;
   pictureElement.querySelector(`.picture__img`).alt = photo.description;
   pictureElement.querySelector(`.picture__likes`).textContent = photo.likes;
@@ -85,58 +83,248 @@ const getPhoto = function (photo) {
 };
 
 const renderPictures = function (pictures) {
-  const picturesElement = document.querySelector(`.pictures`);
   const fragmentPhoto = document.createDocumentFragment();
 
-  pictures.forEach(function (value) {
-    fragmentPhoto.appendChild(getPhoto(value));
-  });
-  picturesElement.appendChild(fragmentPhoto);
+  for (let i = 0; i < pictures.length; i++) {
+    fragmentPhoto.appendChild(getPhoto(pictures[i], i));
+  }
+  return fragmentPhoto;
 };
 
 const mocks = getMocksArray(OBJECTS_AMOUNT);
-renderPictures(mocks);
+picturesElement.appendChild(renderPictures(mocks));
 
-// Задание 3.18 (Личный проект: больше деталей (часть 2))
+// Полноэкранный режим
 
-const createSocialComment = function (object) {
-  const fragment = document.createDocumentFragment();
-  const socialComments = document.querySelector(`.social__comments`);
-  const socialComment = socialComments.querySelector(`li`);
-  socialComments.innerHTML = ``;
-
-  object.comments.forEach(function (value) {
-    const li = socialComment.cloneNode(true);
-    li.querySelector(`.social__picture`).src = value.avatar;
-    li.querySelector(`.social__picture`).alt = value.name;
-    li.querySelector(`.social__picture`).width = AVATAR.width;
-    li.querySelector(`.social__picture`).height = AVATAR.height;
-    li.querySelector(`.social__text`).textContent = value.message;
-    fragment.append(li);
-  });
-  socialComments.append(fragment);
+const renderBigPictureComments = function (comments, mountingPoint) {
+  for (let i = 0; i < comments.length; i++) {
+    const commentListItem = document.createElement(`li`);
+    commentListItem.classList.add(`social__comment`);
+    const commentImage = document.createElement(`img`);
+    commentImage.setAttribute(`src`, `${comments[i].avatar}`);
+    commentImage.setAttribute(`alt`, `${comments[i].name}`);
+    commentImage.setAttribute(`width`, `35`);
+    commentImage.setAttribute(`height`, `35`);
+    commentImage.classList.add(`social__picture`);
+    const commentText = document.createElement(`p`);
+    commentText.classList.add(`social__text`);
+    commentText.textContent = comments[i].message;
+    commentListItem.appendChild(commentImage);
+    commentListItem.appendChild(commentText);
+    mountingPoint.appendChild(commentListItem);
+  }
 };
 
-
-const showFullSizePicture = function (object) {
+const renderBigPicture = function (photo) {
   const bigPicture = document.querySelector(`.big-picture`);
   bigPicture.classList.remove(`hidden`);
-
-  const {url, likes, comments, description} = object;
-  document.querySelector(`.big-picture__img img`).src = url;
-  document.querySelector(`.likes-count`).textContent = likes;
-  document.querySelector(`.comments-count`).textContent = comments.length;
-  document.querySelector(`.social__caption`).textContent = description;
-
-  createSocialComment(mocks[0]);
-
+  bigPicture.querySelector(`.big-picture__img img`).setAttribute(`src`, `${photo.url}`);
+  bigPicture.querySelector(`.likes-count`).textContent = photo.likes;
+  bigPicture.querySelector(`.comments-count`).textContent = photo.comments.length;
+  bigPicture.querySelector(`.social__caption`).textContent = photo.description;
+  bigPicture.querySelector(`.social__comments`).innerHTML = ``;
+  renderBigPictureComments(photo.comments, bigPicture.querySelector(`.social__comments`));
   const socialCommentCount = document.querySelector(`.social__comment-count`);
   socialCommentCount.classList.add(`hidden`);
-
   const commentsLoader = document.querySelector(`.comments-loader`);
   commentsLoader.classList.add(`hidden`);
-
-  document.querySelector(`body`).classList.add(`modal-open`);
+  const body = document.querySelector(`body`);
+  body.classList.add(`modal-open`);
 };
 
-showFullSizePicture(mocks[0]);
+// Скрыть полноэкранный режим
+
+const hideBigPicture = () => {
+  document.querySelector(`.big-picture`).classList.add(`hidden`);
+};
+
+const bigPictureCancel = document.querySelector(`.big-picture__cancel`);
+
+const onBigPictureCancelClick = function () {
+  hideBigPicture();
+
+  picturesElement.addEventListener(`click`, onPhotosContainerClick);
+  bigPictureCancel.removeEventListener(`click`, onBigPictureCancelClick);
+  document.removeEventListener(`keydown`, onBigPictureEscapePress);
+};
+
+const onBigPictureEscapePress = function (evt) {
+  if (evt.key === `Escape`) {
+    evt.preventDefault();
+    hideBigPicture();
+
+    picturesElement.addEventListener(`click`, onPhotosContainerClick);
+    bigPictureCancel.removeEventListener(`click`, onBigPictureCancelClick);
+    document.removeEventListener(`keydown`, onBigPictureEscapePress);
+  }
+};
+
+const openBigPicture = function (id) {
+  renderBigPicture(mocks[id]);
+
+  picturesElement.removeEventListener(`click`, onPhotosContainerClick);
+  bigPictureCancel.addEventListener(`click`, onBigPictureCancelClick);
+  document.addEventListener(`keydown`, onBigPictureEscapePress);
+};
+
+const onPhotosContainerClick = function (evt) {
+  if (evt.target.matches(`.picture__img`)) {
+    openBigPicture(evt.target.parentElement.dataset.id);
+  } else if (evt.target.matches(`.pictures a`)) {
+    openBigPicture(evt.target.dataset.id);
+  }
+};
+
+picturesElement.addEventListener(`click`, onPhotosContainerClick);
+
+//  Задание 4.12 (Личный проект: доверяй, но проверяй (часть 1))
+
+// Загрузка изображения и показ формы редактирования 1.2 и 1.3
+
+const photoEdit = document.querySelector(`.img-upload__overlay`);
+const photoPrew = document.querySelector(`.img-upload__preview img`);
+const effectField = document.querySelector(`.img-upload__effect-level`);
+const uploadFile = document.querySelector(`#upload-file`);
+const uploadCloseBtn = document.querySelector(`.img-upload__cancel`);
+
+const onPhotoEditEscPress = function (evt) {
+  if (evt.key === `Escape`) {
+    evt.preventDefault();
+    photoEdit.classList.add(`hidden`);
+    document.querySelector(`body`).classList.remove(`modal-open`);
+  }
+};
+
+const photoEditOpen = function () {
+  photoEdit.classList.remove(`hidden`);
+  effectField.classList.add(`hidden`);
+  document.querySelector(`body`).classList.add(`modal-open`);
+  document.addEventListener(`keydown`, onPhotoEditEscPress);
+};
+
+const photoEditClose = function () {
+  photoPrew.className = ``;
+  photoPrew.style.transform = ``;
+  photoEdit.classList.add(`hidden`);
+  document.querySelector(`body`).classList.remove(`modal-open`);
+  document.removeEventListener(`keydown`, onPhotoEditEscPress);
+  effectField.classList.add(`hidden`);
+};
+
+uploadCloseBtn.addEventListener(`click`, function () {
+  photoEditClose();
+});
+
+uploadFile.addEventListener(`change`, function () {
+  photoEditOpen();
+
+  if (uploadFile.files && uploadFile.files[0]) {
+    const reader = new FileReader();
+    reader.onload = function (e) {
+      photoPrew.setAttribute(`src`, e.target.result);
+    };
+    reader.readAsDataURL(uploadFile.files[0]);
+  }
+});
+
+// Масштаб
+
+const scaleBtnMin = document.querySelector(`.scale__control--smaller`);
+const scaleBtnMax = document.querySelector(`.scale__control--bigger`);
+const scaleControlNum = document.querySelector(`.scale__control--value`);
+let scale = 100;
+
+scaleBtnMin.addEventListener(`click`, function (evt) {
+  evt.preventDefault();
+  if (scale > 25) {
+    scale -= 25;
+    scaleControlNum.value = `${scale}%`;
+    photoPrew.style.transform = `scale(${scale / 100})`;
+  }
+});
+
+scaleBtnMax.addEventListener(`click`, function (evt) {
+  evt.preventDefault();
+  if (scale < 100) {
+    scale += 25;
+    scaleControlNum.value = `${scale}%`;
+    photoPrew.style.transform = `scale(${scale / 100})`;
+  }
+});
+
+// Эффект на изображение
+
+const img = document.querySelector(`.img-upload__preview img`);
+const effects = document.querySelector(`.effects`);
+
+const onEffectChange = function (evt) {
+  if (evt.target.matches(`input[type="radio"]`)) {
+    img.className = ``;
+    img.className = `effects__preview--${evt.target.value}`;
+
+    if (evt.target.value === `none`) {
+      effectField.classList.add(`hidden`);
+    } else {
+      effectField.classList.remove(`hidden`);
+    }
+  }
+};
+
+effects.addEventListener(`click`, onEffectChange);
+
+// Изменение размера изображения
+
+const effectLevelDepth = effectField.querySelector(`.effect-level__depth`);
+const effectPin = effectField.querySelector(`.effect-level__pin`);
+const effectPinValue = effectField.querySelector(`.effect-level__value`);
+
+effectPin.addEventListener(`mousedown`, function (evt) {
+  const pinLineWidth = effectField.querySelector(`.effect-level__line`).offsetWidth;
+  let startCoord = evt.clientX;
+  const onDocumentMouseMove = function (moveEvt) {
+    const shift = startCoord - moveEvt.clientX;
+    startCoord = moveEvt.clientX;
+    let currentCoord = effectPin.offsetLeft - shift;
+    if (currentCoord < 0) {
+      currentCoord = 0;
+    } else if (currentCoord > pinLineWidth) {
+      currentCoord = pinLineWidth;
+    }
+    effectPin.style.left = `${currentCoord}px`;
+    effectLevelDepth.style.width = `${currentCoord}px`;
+    effectPinValue.value = Math.round(currentCoord * 100 / pinLineWidth);
+  };
+  const onDocumentMouseUp = function () {
+    document.removeEventListener(`mousemove`, onDocumentMouseMove);
+    document.removeEventListener(`mouseup`, onDocumentMouseUp);
+  };
+  document.addEventListener(`mousemove`, onDocumentMouseMove);
+  document.addEventListener(`mouseup`, onDocumentMouseUp);
+});
+
+// Валидация хеш-тегов и комментов
+
+const isRepeated = function (elements) {
+  return Array.from(new Set(elements.map((tag) => tag.toLowerCase()))).length !== elements.length;
+};
+
+const doValidationOfHashtags = function (tags) {
+  const validationRule = /(^|\B)#(?![0-9_]+\b)([a-zA-Z0-9_]{1,20})(\b|\r)/gi;
+  return tags.every((tag) => validationRule.test(tag));
+};
+
+const hashTagsInput = document.querySelector(`.text__hashtags`);
+
+const hashTagsInputKeyupHandler = function (evt) {
+  const hashTags = evt.target.value.split(` `);
+  if (hashTags.length > 5) {
+    hashTagsInput.setCustomValidity(`Максимальное количество тегов - 5`);
+  } else if (isRepeated(hashTags)) {
+    hashTagsInput.setCustomValidity(`Теги не должны повторяться`);
+  } else if (doValidationOfHashtags(hashTags)) {
+    hashTagsInput.setCustomValidity(`Теги должны соответствовать формату`);
+  } else {
+    hashTagsInput.setCustomValidity(``);
+  }
+};
+hashTagsInput.addEventListener(`input`, hashTagsInputKeyupHandler);

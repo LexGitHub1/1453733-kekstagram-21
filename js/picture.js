7555f2607828828f2087a6ad357b6620124cfcdc
Alexander Pankov <alexander.pankov88@mail.ru>
2020-11-10 20:27:49 +0700

    Исправление server и др.

--- js/picture.js
+++ js/picture.js
@@ -1,30 +1,65 @@
 'use strict';
 
 (function () {
-  const getPhoto = function (photo, id) {
-    const pictureTemplate = document.querySelector(`#picture`).content.querySelector(`.picture`);
-    const pictureElement = pictureTemplate.cloneNode(true);
+  const pictures = document.querySelector(`.pictures`);
+  const filters = document.querySelector(`.img-filters`);
+  const openBigPicturePopup = window.bigImgShow.openBigPicture;
 
-    pictureElement.dataset.id = id;
-    pictureElement.querySelector(`.picture__img`).src = photo.url;
-    pictureElement.querySelector(`.picture__img`).alt = photo.description;
-    pictureElement.querySelector(`.picture__likes`).textContent = photo.likes;
-    pictureElement.querySelector(`.picture__comments`).textContent = photo.comments.length;
+  const template = document.querySelector(`#picture`)
+    .content
+    .querySelector(`.picture`);
 
-    return pictureElement;
+  const createCardElement = function (object) {
+    const {likes, comments, url} = object;
+    const cardElement = template.cloneNode(true);
+    cardElement.querySelector(`.picture__likes`).textContent = likes;
+    cardElement.querySelector(`.picture__comments`).textContent = comments.length;
+    cardElement.querySelector(`img`).src = url;
+    return cardElement;
   };
 
-  const renderPictures = function () {
-    const pictures = document.querySelector(`.pictures`);
-    const fragmentPhoto = document.createDocumentFragment();
-
-    window.mockscreation.mocks.forEach(function (value) {
-      fragmentPhoto.appendChild(getPhoto(value));
+  const renderPictures = function (cardsArray) {
+    const fragment = document.createDocumentFragment();
+    cardsArray.forEach(function (cardObject) {
+      fragment.appendChild(createCardElement(cardObject));
     });
-    pictures.appendChild(fragmentPhoto);
-
-    return fragmentPhoto;
+    pictures.appendChild(fragment);
+    return fragment;
   };
 
-  renderPictures(window.mockscreation.mocks);
+  const errorHandler = function (errorMessage) {
+    const node = document.createElement(`div`);
+    node.style = `z-index: 100; margin: 0 auto; text-align: center; background-color: red;`;
+    node.style.position = `absolute`;
+    node.style.left = 0;
+    node.style.right = 0;
+    node.style.fontSize = `30px`;
+    node.textContent = errorMessage;
+    document.body.insertAdjacentElement(`afterbegin`, node);
+  };
+
+  const clickSmallPhoto = function (data) {
+    const smallPhotosList = document.querySelectorAll(`.picture`);
+    smallPhotosList.forEach(function (picture, index) {
+      picture.addEventListener(`click`, function (evt) {
+        evt.preventDefault();
+        openBigPicturePopup(data[index]);
+      });
+    });
+  };
+
+  const successDataLoadHandler = function (data) {
+    window.cardCreate.cardsList = [];
+    window.cardCreate.cardsList = data;
+    renderPictures(data);
+    clickSmallPhoto(data);
+    filters.classList.remove(`img-filters--inactive`);
+  };
+
+  window.server.load(successDataLoadHandler, errorHandler);
+
+  window.cardCreate = {
+    renderPictures,
+    clickSmallPhoto
+  };
 })();
--- js/server.js
+++ js/server.js
@@ -1,23 +1,20 @@
-'use strict';
+"use strict";
 
 (function () {
-
-  const URL = {
-    load: `https://21.javascript.pages.academy/kekstagram/data`,
-    upload: `https://21.javascript.pages.academy/kekstagram`
-  };
-
-  const STATUS_CODE = {
-    OK: 200
-  };
+  const URL_LOAD = `https://21.javascript.pages.academy/kekstagram/data`;
+  const URL_UPLOAD = `https://21.javascript.pages.academy/kekstagram/`;
 
   const TIMEOUT_IN_MS = 10000;
 
-  const server = function (xhr, onSuccess, onError) {
+  const StatusCode = {
+    OK: 200
+  };
+
+  const getServer = function (xhr, onSuccess, onError) {
     xhr.responseType = `json`;
 
     xhr.addEventListener(`load`, function () {
-      if (xhr.status === STATUS_CODE.OK) {
+      if (xhr.status === StatusCode.OK) {
         onSuccess(xhr.response);
       } else {
         onError(`Статус ответа: ` + xhr.status + ` ` + xhr.statusText);
@@ -35,15 +32,15 @@
 
   const load = function (success, error) {
     const xhr = new XMLHttpRequest();
-    xhr.open(`GET`, URL.load);
-    server(xhr, success, error);
+    xhr.open(`GET`, URL_LOAD);
+    getServer(xhr, success, error);
     xhr.send();
   };
 
   const upload = function (data, success, error) {
     const xhr = new XMLHttpRequest();
-    xhr.open(`POST`, URL.upload);
-    server(xhr, success, error);
+    xhr.open(`POST`, URL_UPLOAD);
+    getServer(xhr, success, error);
     xhr.send(data);
   };
 
